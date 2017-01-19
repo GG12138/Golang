ce74a179f42f130c1dda9d44e63ac6690fe10245
zhukai <1842159520@qq.com>
2017-01-19 17:49:08 +0800

    channel 属性

--- show/show.go
+++ show/show.go
@@ -6,9 +6,11 @@ type Show struct {
 	name string
 	age int
 }
+//msg传进来string类型数据 放进pings通道
 func (s *Show) Ping(pings chan<- string,msg string){
 	pings <- msg
 }
+//pings 接收的数据放进msg，msg将数据放进pongs通道
 func (s *Show) Pong(pings <-chan string,pongs chan <- string){
 	msg:= <- pings
 	pongs <- msg
--- t.go
+++ t.go
@@ -11,8 +11,12 @@ func main(){
 
 	pings := make(chan string,1)
 	pongs := make(chan string,1)
+	//接收回来的数据保存在pings通道里
+	//如果pings不取出来 将报错 **all goroutines are asleep - deadlock!**
 	show.Ping(pings,"msg")
+	//pings放进Pong方法，赋值给pongs通道
 	show.Pong(pings,pongs)
+	//最后取出 <-pongs  输出 msg
 	fmt.Println(<-pongs)
 
 }
